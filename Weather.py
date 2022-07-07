3f412045e7226d7575922e6eba03cc0d428a05f9
Signar Rasmussen <signar76@gmail.com>
2022-07-07 14:57:57 +0100

    Add files via upload

---
+++ Weather.py
@@ -0,0 +1,10 @@
+temperature = 35
+
+if temperature > 35:
+    print("It's too hot!")
+    print("Stay inside!")
+elif temperature < 15:
+    print("It's too cold!")
+    print("Stay inside!")
+else:
+    print("Enjoy the outdoors!")
