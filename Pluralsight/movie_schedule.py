5e6748c6e4c3baa5509c6f5b3c6446f48c1e14dc
Signar Rasmussen <signar76@gmail.com>
2022-07-13 12:59:08 +0100

    lists

--- Pluralsight/movie_schedule.py
+++ Pluralsight/movie_schedule.py
@@ -3,4 +3,13 @@ current_movies = {'The Grinch':'11:00am',
                   'Frosty the Snowman':'3:00pm',
                   'Christmas Vacation':'5:00pm'}
 
-current_movies = {}
+print("We're showing the following movies:")
+for key in current_movies:
+    print(key)
+movie = input('What movie would you like the showtime for?\n')
+
+showtime = current_movies.get(movie)
+if showtime == None:
+    print("Requested showtime isn't playing")
+else:
+    print(movie, 'is playing at', showtime)
