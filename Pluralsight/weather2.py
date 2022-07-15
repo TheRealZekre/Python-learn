d8162216a73b5367c6bdde5faa67acd133df16c4
Signar Rasmussen <signar76@gmail.com>
2022-07-15 15:28:04 +0100

    rolle the dice

--- Pluralsight/weather2.py
+++ Pluralsight/weather2.py
@@ -1,19 +1,29 @@
 import requests
 
-api_key = "36a9809ddd10721ff33b2d505979d391"
-city = "Orlando"
-url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=metric"
+def get_weather_desc_and_temp ():
+    api_key = "36a9809ddd10721ff33b2d505979d391"
+    city = "Orlando"
+    url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=metric"
 
-request = requests.get(url)
-json = request.json()
+    request = requests.get(url)
+    json = request.json()
 
-description = json.get("weather")[0].get("description")
-print("Today's forecast is", description)
+    description = json.get("weather")[0].get("description")
+    temp_min = json.get("main").get("temp_min")
+    temp_max = json.get("main").get("temp_max")
 
-temp_min = json.get("main").get("temp_min")
-print("The minimum temperature is", temp_min)
+    return {'description': description,
+            'temp_min': temp_min,
+            'temp_max': temp_max}
 
-temp_max = json.get("main").get("temp_max")
-print("The maximum temperature is", temp_max)
+
+def main():
+    # Print the results
+    weather_dict = get_weather_desc_and_temp()
+    print("Today's forecast is", weather_dict.get('description'))
+    print("The minimum temperature is", weather_dict.get('temp_min'))
+    print("The maximum temperature is", weather_dict.get('temp_max'))
+
+main()
 
 
